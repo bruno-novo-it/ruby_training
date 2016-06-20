98616024c6e32afca280e0235f0918f9ee5a1bde
cs-bruno-novo <bruno.novo@concrete.com.br>
2016-06-20 11:18:17 -0300

    Improvement of identation

--- IMC.rb
+++ IMC.rb
@@ -12,17 +12,17 @@ imc = (peso/(altura**2)).round(2)
 
 if imc<17 
 	puts "IMC = #{imc}; Muito abaixo do peso"
-		elsif imc<=18.49
-					puts "IMC = #{imc}; Abaixo do peso"
-							elsif imc<=24.99
-											puts "IMC = #{imc}; Peso normal"
-														elsif imc<=29.99
-																			puts "IMC = #{imc}; Acima do peso"
-																							elsif imc<=34.99
-																													puts "IMC = #{imc}; Obesidade I"
-																																		elsif imc<=39.99
-																																									puts "IMC = #{imc}; Obesidade II (severa)"
-																																															else
-																																																							puts "IMC = #{imc}; Obesidade III (mórbida)"
-																																															end
+elsif imc<=18.49
+	puts "IMC = #{imc}; Abaixo do peso"
+elsif imc<=24.99
+	puts "IMC = #{imc}; Peso normal"
+elsif imc<=29.99
+	puts "IMC = #{imc}; Acima do peso"
+elsif imc<=34.99
+	puts "IMC = #{imc}; Obesidade I"
+elsif imc<=39.99
+	puts "IMC = #{imc}; Obesidade II (severa)"
+else
+	puts "IMC = #{imc}; Obesidade III (mórbida)"
+end
 
