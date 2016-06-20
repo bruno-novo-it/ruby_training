132486e244435fd415ad0d0cff3cd6b65c9d0ca2
cs-bruno-novo <bruno.novo@concrete.com.br>
2016-06-20 14:11:20 -0300

    Create a test file and modified the principal imc archive

--- imc.rb
+++ imc.rb
@@ -1,28 +1,42 @@
-puts "Calculo de IMC"
+class Imc
 
-puts "Informe a sua altura(em metros)(Utilize pontos ao invés de vírgula)"
-#Captura a altura ja transformando-a em ponto flutuante(float)
-altura = gets.to_f
+	def calculo_imc altura,peso
+		
 
-puts "Informe o seu peso em kg (utilize pontos ao invés de vírgula)"
-#Captura o pedo ja transformando-o em ponto flutuante(float)
-peso = gets.to_f
-#Faz o cálculo do peso dividido pela a altura ao quadrado mostrando apenas 2 casas depois da virgula(.round(2))
-imc = (peso/(altura**2)).round(2)
+		#Faz o cálculo do peso dividido pela a altura ao quadrado mostrando apenas 2 casas depois da virgula(.round(2))
+		imc = (peso/(altura**2)).round(2)
+		
+		if imc<17
+			puts "IMC = #{imc}; Muito abaixo do peso"
+		elsif imc<=18.49
+			puts "IMC = #{imc}; Abaixo do peso"
+		elsif imc<=24.99
+			puts "IMC = #{imc}; Peso normal"
+		elsif imc<=29.99
+			puts "IMC = #{imc}; Acima do peso"
+		elsif imc<=34.99
+			puts "IMC = #{imc}; Obesidade I"
+		elsif imc<=39.99
+			puts "IMC = #{imc}; Obesidade II (severa)"
+		else
+			puts "IMC = #{imc}; Obesidade III (mórbida)"
+		end
 
-if imc<17 
-	puts "IMC = #{imc}; Muito abaixo do peso"
-elsif imc<=18.49
-	puts "IMC = #{imc}; Abaixo do peso"
-elsif imc<=24.99
-	puts "IMC = #{imc}; Peso normal"
-elsif imc<=29.99
-	puts "IMC = #{imc}; Acima do peso"
-elsif imc<=34.99
-	puts "IMC = #{imc}; Obesidade I"
-elsif imc<=39.99
-	puts "IMC = #{imc}; Obesidade II (severa)"
-else
-	puts "IMC = #{imc}; Obesidade III (mórbida)"
+	end
+
+	puts "Informe a sua altura(em metros)(Utilize pontos ao invés de vírgula)"
+	#Captura a altura transformando-a em ponto flutuante(float)
+	altura = gets.to_f
+	
+	puts "Informe o seu peso em kg (utilize pontos ao invés de vírgula)"
+	#Captura o peso transformando-o em ponto flutuante(float)
+	peso = gets.to_f
+
+	#Para testar a função criasse uma instancia da classe
+	#imc = Imc.new 
+	# e chama a função calculo_imc 	
+	#imc.calculo_imc altura,peso	
+
+
 end
 
