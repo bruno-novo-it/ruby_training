5848dd6a8a358c53138c20197437fe6681906552
cs-bruno-novo <bruno.novo@concrete.com.br>
2016-06-20 21:59:56 -0300

    The media archives were created and tested

---
+++ media_teste.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require './calculo_media.rb'
+
+
+
+class CalculaMediaTest < Test::Unit::TestCase
+
+  def teste_calcula_media
+    media = Media.new
+    assert_equal(6,media.calculo_media(6,6))
+  end
+
+  def teste_aprovado
+    media = Media.new
+    assert_equal("Aluno Aprovado",media.dilema(6))
+  end
+
+  def teste_reprovado
+    media = Media.new
+    assert_equal("Aluno Reprovado",media.dilema(4))
+  end
+
+
+end
