36cb0fb75e9c848232f4148f522f658eb3160a38
cs-bruno-novo <bruno.novo@concrete.com.br>
2016-06-20 22:26:26 -0300

    Deleted unused files

--- calculo_imc.rb
+++
@@ -1,9 +0,0 @@
-
-
-def calculo_imc altura,peso
-
-  #Faz o cálculo do peso dividido pela a altura ao quadrado mostrando apenas 2
-  #casas depois da virgula(.round(2))
-  imc = (peso/(altura**2)).round(2)
-
-end
